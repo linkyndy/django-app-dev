ab096cb346276854736e0edc1ab9d73676bc7dc8
Andrei Horak <linkyndy@gmail.com>
2014-03-06 16:46:21 +0200

    Fixed type in settings file

--- app/tests/settings.py
+++ app/tests/settings.py
@@ -7,7 +7,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.cotnrib.sites',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
